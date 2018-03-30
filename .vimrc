6621f71d21e4ea8db76ba7cabd517e5b7f5dc8ef
bquigs <bquigs@localhost.localdomain>
2018-03-30 01:24:42 -0700

    Initial commit of working install script for dotfiles
    Kudoes to pdxjohnny for the idea

---
+++ .vimrc
@@ -0,0 +1,31 @@
+call plug#begin('~/.vim/plugged')
+Plug 'airblade/vim-gitgutter'
+Plug 'flazz/vim-colorschemes'
+Plug 'suan/vim-instant-markdown'
+call plug#end()
+
+silent! colorscheme DarkDefault
+hi clear CursorLine
+hi CursorLine cterm=bold ctermbg=233
+
+" Dark grey ruler at 81 character width
+set colorcolumn=81
+highlight ColorColumn ctermbg=233
+set tw=80
+
+" Show line numbers
+set number
+
+" Stop vim from creating auto backup
+set nobackup
+set nowritebackup
+
+" Highlight trailing spaces
+" http://vim.wikia.com/wiki/Highlight_unwanted_spaces
+highlight ExtraWhitespace ctermbg=red guibg=red
+match ExtraWhitespace /\s\+$/
+autocmd BufWinEnter * match ExtraWhitespace /\s\+$/
+autocmd InsertEnter * match ExtraWhitespace /\s\+\%#\@<!$/
+autocmd InsertLeave * match ExtraWhitespace /\s\+$/
+autocmd BufWinLeave * call clearmatches()
+
---
+++ install.sh
@@ -0,0 +1,68 @@
+#!/bin/sh
+
+usage_error() {
+	local error_type=$1
+	local error_text=$2
+
+	if [ -z "$error_type" ]; then
+		echo "Error: usage_error() requires an error_type argument"
+		exit 1
+	fi
+
+	if [ -z "$error_text" ]; then
+		echo "Error: usage_error() requires an error_text argument"
+		exit 1
+	fi
+
+	case $error_type in
+		missing_prog_error)
+			echo "Error: Install $error_text to proceed" ;;
+		github_pull_error)
+			echo "Error: Failed to pull $error_text from github" ;;
+		file_copy_error)
+			echo "Error: Failed to copy $error_text to $HOME/" ;;
+		vim_setup_error)
+			echo "Error: Failed to setup $error_text" ;;
+		*)
+			echo "install.sh encountered unknown error ..." ;;
+	esac
+
+	exit 1
+}
+
+# Make sure git, curl, and vim are installed and keep stdout quiet
+which git > /dev/null
+if [ $? -ne 0 ]; then
+	usage_error "missing_prog_error" "git"
+fi
+
+which curl > /dev/null
+if [ $? -ne 0 ]; then
+	usage_error "missing_prog_error" "curl"
+fi
+
+which vim > /dev/null
+if [ $? -ne 0 ]; then
+	usage_error "missing_prog_error" "vim"
+fi
+
+# Try to copy all .* files if they are newer or don't exist to /home/username
+cp -r ./.vim* -t $HOME/
+if [ $? -ne 0 ]; then
+	usage_error "file_copy_error" "$PWD/*"
+fi
+
+# Try to git vim-plug from GitHub and make it quiet
+curl -fLo ~/.vim/autoload/plug.vim \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim \
+    > /dev/null 2>&1
+if [ $? -ne 0 ]; then
+	usage_error "github_pull_error" "plug.vim"
+fi
+
+vim +'PlugInstall --sync' +qa
+if [ $? -ne 0 ]; then
+	usage_error "vim_setup_error" "vim-plug plugins"
+fi
+
+echo "install.sh success, dotfiles are configured!"
